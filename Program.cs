8946c3f0a2ac5dcfebbba6c3c0b6f0529a2276a5
jeroenslemmer <jslemmer@davinci.nl>
2020-05-13 13:04:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokeBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
